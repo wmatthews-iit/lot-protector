e1bfe5103307128e6fb9749f4a90c406f355a844
Wesley Matthews <wmatthews@hawk.iit.edu>
2025-03-14 17:28:10 -0500

    Added SMS verification

--- app/finish/Finish.tsx
+++ app/finish/Finish.tsx
@@ -3,18 +3,24 @@
 import SignInForm from '@/components/SignInForm';
 import SignUpForm from '@/components/SignUpForm';
 import { auth, functions } from '@/lib/firebase/app';
-import { Modal, Text, Title } from '@mantine/core';
+import { Box, Button, Modal, Text, TextInput, Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { isSignInWithEmailLink, signInWithEmailLink, updateProfile } from 'firebase/auth';
+import { getMultiFactorResolver, isSignInWithEmailLink, multiFactor, PhoneAuthProvider, PhoneMultiFactorGenerator, RecaptchaVerifier, signInWithEmailLink, updateProfile } from 'firebase/auth';
 import { httpsCallable } from 'firebase/functions';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Finish() {
-  const router = useRouter();
   const searchParams = useSearchParams();
   const isSignUp = searchParams.has('signup');
   const [detailsOpened, { toggle: toggleDetails, close: closeDetails }] = useDisclosure(false);
+  const [finished, setFinished] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [textSent, setTextSent] = useState(false);
+  const [verificationID, setVerificationID] = useState<any>(null);
+  const [code, setCode] = useState('');
+  const [recaptchaVerifier, setRecaptchaVerifier] = useState<RecaptchaVerifier | null>(null);
+  const [resolver, setResolver] = useState<any>(null);
   
   const tryToFinish = async ({ name, email, role }:
     { name?: string, email?: string, role?: string }) => {
@@ -47,11 +53,11 @@ export default function Finish() {
           }
         }
         
-        const user = await userCredential.user.getIdTokenResult();
-        window.location.replace(user.claims.isManager ? '/live' : '/find');
+        setFinished(true);
       }
-    } catch (error) {
-      console.log(error);
+    } catch (error: any) {
+      if (error.code === 'auth/multi-factor-auth-required') await sendText(error);
+      else console.log(error);
     }
   };
   
@@ -59,18 +65,109 @@ export default function Finish() {
     let ignore = false;
     
     (async () => {
-      if (!ignore) tryToFinish({});
+      if (!ignore) {
+        setRecaptchaVerifier(new RecaptchaVerifier(auth, 'complete-button', {
+          size: 'invisible',
+          callback: (response: any) => {},
+        }));
+        
+        if (isSignUp) tryToFinish({});
+      }
     })();
     
     return () => { ignore = true };
   }, []);
   
+  const sendText = async (error: any) => {
+    if (!recaptchaVerifier || isSignUp && !auth.currentUser) return;
+    
+    try {
+      if (isSignUp) {
+        const multiFactorSession = await multiFactor(auth.currentUser!).getSession();
+        const phoneAuthProvider = new PhoneAuthProvider(auth);
+        
+        setVerificationID(await phoneAuthProvider.verifyPhoneNumber({
+          phoneNumber,
+          session: multiFactorSession,
+        }, recaptchaVerifier));
+      } else {
+        const resolver = getMultiFactorResolver(auth, error);
+        const phoneAuthProvider = new PhoneAuthProvider(auth);
+        const verificationID = await phoneAuthProvider
+          .verifyPhoneNumber({ session: resolver.session, multiFactorHint: resolver.hints[0] }, recaptchaVerifier);
+        setVerificationID(verificationID);
+        setResolver(resolver);
+      }
+      
+      setTextSent(true);
+    } catch (error) {
+      console.log(error);
+      recaptchaVerifier.clear();
+      setTextSent(false);
+      setVerificationID(null);
+    }
+  };
+  
+  const confirmNumber = async () => {
+    if (!recaptchaVerifier || isSignUp && !auth.currentUser) return;
+    
+    try {
+      if (isSignUp) {
+        const phoneCredential = PhoneAuthProvider.credential(verificationID, code);
+        const multiFactorAssertion = PhoneMultiFactorGenerator.assertion(phoneCredential);
+        await multiFactor(auth.currentUser!).enroll(multiFactorAssertion, 'phone');
+      }
+      else {
+        const phoneCredential = PhoneAuthProvider.credential(verificationID, code);
+        const multiFactorAssertion = PhoneMultiFactorGenerator.assertion(phoneCredential);
+        await resolver.resolveSignIn(multiFactorAssertion);
+      }
+      
+      const token = await auth.currentUser!.getIdTokenResult(true);
+      window.location.replace(token.claims.isManager ? '/live' : '/find');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   return <>
     <Title
       mb="md"
       order={1}
-    >Finishing Sign {isSignUp ? 'Up' : 'In'}</Title>
-    <Text>Finishing {isSignUp ? 'creating' : 'signing into'} your account...please wait</Text>
+    >Finish{finished ? 'ed' : 'ing'} Sign {isSignUp ? 'Up' : 'In'}</Title>
+    <Text display={(!isSignUp || finished) ? 'none' : 'block'}>Finishing {isSignUp ? 'creating' : 'signing into'} your account...please wait</Text>
+    <Box display={(!isSignUp || finished) && !textSent ? 'block' : 'none'}>
+      <Text mb="md">Please complete two factor authentication through a text message. Standard rates will apply</Text>
+      <TextInput
+        data-autofocus
+        description="Format: +10123456789"
+        display={isSignUp ? 'block' : 'none'}
+        label="Phone Number"
+        mb="sm"
+        onChange={(event) => setPhoneNumber(event.currentTarget.value)}
+        value={phoneNumber}
+      />
+      <Button
+        disabled={isSignUp && phoneNumber.length === 0}
+        id="complete-button"
+        onClick={isSignUp ? sendText : (() => tryToFinish({}))}
+      >Send Text</Button>
+    </Box>
+    
+    <Box display={(!isSignUp || finished) && textSent ? 'block' : 'none'}>
+      <TextInput
+        data-autofocus
+        label="Code"
+        mb="sm"
+        onChange={(event) => setCode(event.currentTarget.value)}
+        value={code}
+      />
+      <Button
+        disabled={isSignUp && phoneNumber.length === 0}
+        onClick={confirmNumber}
+      >Confirm Number</Button>
+    </Box>
+    
     <Modal
       centered
       onClose={closeDetails}
